69b9e4cd62167831b80e0a77ecfa304d889baf4b
Prashant Hule <prashant.hule6999@gmail.com>
2026-06-17 16:43:07 +0530

    Data ingeston completed

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -1,16 +1,14 @@
 # data ingestion
-from src.logger import logging
-import logging
+
 import yaml
-from sklearn.model_selection import train_test_split
-import os
 import kagglehub
-import numpy as np
 import pandas as pd
-pd.set_option('future.no_silent_downcasting', True)
+from typing import Any
+from pathlib import Path
+from src.logger import logging
 
 
-def load_params(params_path: str) -> dict:
+def load_params(params_path: str) -> dict[str, Any]:
     """Load parameters from a YAML file."""
     try:
         with open(params_path, 'r') as file:
@@ -28,14 +26,30 @@ def load_params(params_path: str) -> dict:
         raise
 
 
-def load_data(data_url: str, fileName: str) -> pd.DataFrame:
+def load_data(data_url: str, file_name: str) -> pd.DataFrame:
     """Load data from kaggle site."""
     try:
-        path = kagglehub.dataset_download(data_url)
-        csv_file = os.path.join(path, fileName)
+        path = Path(kagglehub.dataset_download(data_url))
+        csv_file = path / file_name
+        if not csv_file.exists():
+            raise FileNotFoundError(
+                f"Dataset file not found: {csv_file}"
+            )
+
         df = pd.read_csv(csv_file)
-        logging.info('Data loaded from %s/%s', data_url, fileName)
+
+        if df.empty:
+            raise ValueError("Loaded dataset is empty")
+        logging.info('Data loaded from %s/%s', data_url, file_name)
+
+        logging.info(
+            "Dataset contains. Rows=%d Columns=%d",
+            df.shape[0],
+            df.shape[1]
+        )
+
         return df
+
     except pd.errors.ParserError as e:
         logging.error('Failed to parse the CSV file: %s', e)
         raise
@@ -45,59 +59,42 @@ def load_data(data_url: str, fileName: str) -> pd.DataFrame:
         raise
 
 
-def preprocess_data(df: pd.DataFrame, cols: list[str]) -> pd.DataFrame:
-    """Replace zeros with NaN in specified columns."""
+def save_data(df: pd.DataFrame, file_name: str, data_path: str) -> None:
+    """Save datasets."""
     try:
-        logging.info("Pre-processing data...")
+        raw_data_path = Path(data_path) / "raw"
+        raw_data_path.mkdir(parents=True, exist_ok=True)
 
-        final_df = df.copy()
-        final_df[cols] = final_df[cols].replace(0, np.nan)
+        output_file = raw_data_path / file_name
+        df.to_csv(output_file, index=False)
 
-        logging.info('Data preprocessing completed')
-        return final_df
-    except KeyError as e:
-        logging.error('Missing column in the dataframe: %s', e)
-        raise
-    except Exception as e:
-        logging.error('Unexpected error during preprocessing: %s', e)
-        raise
+        logging.info(
+            "Dataset saved successfully at %s",
+            output_file
+        )
 
-
-def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str) -> None:
-    """Save the train and test datasets."""
-    try:
-        raw_data_path = os.path.join(data_path, 'raw')
-        os.makedirs(raw_data_path, exist_ok=True)
-        train_data.to_csv(os.path.join(
-            raw_data_path, "train.csv"), index=False)
-        test_data.to_csv(os.path.join(raw_data_path, "test.csv"), index=False)
-        logging.debug('Train and test data saved to %s', raw_data_path)
     except Exception as e:
         logging.error('Unexpected error occurred while saving the data: %s', e)
         raise
 
 
-def main():
+def main() -> None:
+
     try:
-        params = load_params(params_path='params.yaml')
-        test_size = params['data_ingestion']['test_size']
-        random_state = params['data_ingestion']['random_state']
-        data_url = params['data_ingestion']['data_url']
-        fileName = params['data_ingestion']['fileName']
+        params = load_params('params.yaml')
+
+        data_url = params["data_ingestion"]['data_url']
+        file_name = params["data_ingestion"]['file_name']
+        raw_data_path = params["data_ingestion"]["raw_data_path"]
 
         df = load_data(
-            data_url=data_url, fileName=fileName)
-        cols = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']
-        final_df = preprocess_data(df, cols)
+            data_url, file_name)
 
-        train_data, test_data = train_test_split(
-            final_df, test_size=test_size, random_state=42)
-
-        save_data(train_data, test_data, data_path='./data')
+        save_data(df, file_name, raw_data_path)
 
     except Exception as e:
         logging.error('Failed to complete the data ingestion process: %s', e)
-        print(f"Error: {e}")
+        raise
 
 
 if __name__ == '__main__':
